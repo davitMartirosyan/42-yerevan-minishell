8931f3f88c5fb6953cd62fd3f060f7d2fe1537e5
davitMartirosyan <dmartiro@c1r7s4.42yerevan.am>
2022-12-02 07:36:53 +0400

    executable files

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: dmartiro <dmartiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 20:19:57 by dmartiro          #+#    #+#             */
-/*   Updated: 2022/12/02 07:17:30 by dmartiro         ###   ########.fr       */
+/*   Updated: 2022/12/02 07:36:42 by dmartiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,8 @@ int main(int argc, char *argv[], char *envp[])
     while(1)
     {
         cmdline = readline("Minishell-$ ");
+		if(ft_strncmp(cmdline, "exit", ft_strlen("exit")) == 0)
+			exit(1);
         add_history(cmdline);
         lexical_analyzer(cmdline, table);
         tree = parse_tree(table, envp);
